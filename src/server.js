3b2bb2e55eeaefa3a24640410734d25b5fbc72ab
Daniel Loeffelholz <dloeffel@thoughtworks.com>
2019-06-01 15:35:33 +0200

    fixing port

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = parseInt(process.env.PORT, 10) || 3000;
 
 app.get('/', (req, res) => res.send("{message: 'Hello World'}"));
 
 let server = app.listen(port, () => {
-        console.log(`Listening on port ${port}!`);
+        console.log(`Listening on port ${PORT}!`);
     }
 );
 
